4257f5d50ada810679084d6d89f9556fd8bd5309
An-109 <doggod232471@gmail.com>
2025-01-24 16:18:35 -0700

    please work

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -30,11 +30,11 @@ const startApolloServer = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../Client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
+      res.sendFile(path.join(__dirname, '../../Client/dist/index.html'));
+});
   }
 
   app.listen(PORT, () => {
