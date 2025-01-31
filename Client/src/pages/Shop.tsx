74e788ac0ffe195d3f3868463753e2596ee5f005
Martha Watson <marthacdenzer@gmail.com>
2025-01-30 20:54:49 -0700

    tuched up some code code in shop

--- Client/src/pages/Shop.tsx
+++ Client/src/pages/Shop.tsx
@@ -31,7 +31,7 @@ function Shop() {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 place-items-center">
                     {productData.map((product) => (
-                        <div key={product._id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-blue-400 dark:border-blue-700">
+                        <div key={product._id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-blue-400">
                             <a href="#">
                                 <div className="w-full h-96 overflow-hidden">
                                     <img className="w-full h-full object-cover rounded-t-lg" src={product.image} alt={product.name} />
@@ -45,7 +45,7 @@ function Shop() {
 
                                 <div className="flex items-center justify-between mt-4">
                                     <span className="text-2xl font-bold text-gray-900 dark:text-white">${product.price}</span>
-                                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-400">
                                         Add to cart
                                     </a>
                                 </div>
