80bf4c9a9d36a0f82d3f9b032b5d731bde670e4d
fagan921 <fagan921@gmail.com>
2025-01-30 20:57:25 -0800

    unused imports

--- Client/src/pages/FindUs.tsx
+++ Client/src/pages/FindUs.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Layout from '../components/Layout';
+// import React from 'react';
+// import Layout from '../components/Layout';
 import Map from '../components/Map';
 
 function FindUs() {
